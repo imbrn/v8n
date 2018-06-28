eb318eda9c261a14d6471407e511bb7506a881d4
Bruno <brunodev02221@gmail.com>
2018-06-28 13:53:35 -0300

    Improve tests
    * Move 'boolean' rule tests selection to near of all other types tests
    section.

--- src/v8n.test.js
+++ src/v8n.test.js
@@ -166,6 +166,16 @@ describe("rules", () => {
     expect(validation.test(undefined)).toBeFalsy();
   });
 
+  test("boolean", () => {
+    const validation = v8n().boolean();
+    expect(validation.test(true)).toBeTruthy();
+    expect(validation.test(false)).toBeTruthy();
+    expect(validation.test(1)).toBeFalsy();
+    expect(validation.test(0)).toBeFalsy();
+    expect(validation.test(null)).toBeFalsy();
+    expect(validation.test(undefined)).toBeFalsy();
+  });
+
   test("lowercase", () => {
     const validation = v8n().lowercase();
     expect(validation.test("")).toBeFalsy();
@@ -314,16 +324,6 @@ describe("rules", () => {
     expect(validation.test(0)).toBeFalsy();
     expect(validation.test(1)).toBeTruthy();
     expect(validation.test(2)).toBeFalsy();
-  });
-
-  test("boolean", () => {
-    const validation = v8n().boolean();
-    expect(validation.test(true)).toBeTruthy();
-    expect(validation.test(false)).toBeTruthy();
-    expect(validation.test(1)).toBeFalsy();
-    expect(validation.test(0)).toBeFalsy();
-    expect(validation.test(null)).toBeFalsy();
-    expect(validation.test(undefined)).toBeFalsy();
   });
 
   test("between", () => {

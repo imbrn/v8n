7319a2dd262582d25e2cae571f3459adf692f2a4
Bruno C. Couto <brunodev02221@gmail.com>
2018-08-01 12:59:11 -0300

    Change some function and variable names
    * Rename `testAux` to `decorateFn`
    * Rename `testAuxAsync` to `decorateFnAsync`

--- src/Rule.js
+++ src/Rule.js
@@ -16,7 +16,7 @@ class Rule {
     }
 
     try {
-      return testAux(this.modifiers.slice(), fn)(value);
+      return decorateFn(this.modifiers.slice(), fn)(value);
     } catch (ex) {
       return false;
     }
@@ -26,19 +26,19 @@ class Rule {
     try {
       this.fn(value);
     } catch (ex) {
-      if (testAux(this.modifiers.slice(), it => it)(false)) {
+      if (decorateFn(this.modifiers.slice(), it => it)(false)) {
         return;
       }
     }
 
-    if (!testAux(this.modifiers.slice(), this.fn)(value)) {
+    if (!decorateFn(this.modifiers.slice(), this.fn)(value)) {
       throw null;
     }
   }
 
   _testAsync(value) {
     return new Promise((resolve, reject) => {
-      testAsyncAux(this.modifiers.slice(), this.fn)(value)
+      decorateFnAsync(this.modifiers.slice(), this.fn)(value)
         .then(valid => {
           if (valid) {
             resolve(value);
@@ -51,20 +51,20 @@ class Rule {
   }
 }
 
-function testAux(modifiers, fn) {
+function decorateFn(modifiers, fn) {
   if (modifiers.length) {
     const modifier = modifiers.shift();
-    const nextFn = testAux(modifiers, fn);
+    const nextFn = decorateFn(modifiers, fn);
     return modifier.perform(nextFn);
   } else {
     return fn;
   }
 }
 
-function testAsyncAux(modifiers, fn) {
+function decorateFnAsync(modifiers, fn) {
   if (modifiers.length) {
     const modifier = modifiers.shift();
-    const nextFn = testAsyncAux(modifiers, fn);
+    const nextFn = decorateFnAsync(modifiers, fn);
     return modifier.performAsync(nextFn);
   } else {
     return value => Promise.resolve(fn(value));

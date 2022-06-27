86a87eb16b597588f204047f1340112e39ec7e30
imbrn <brunodev02221@gmail.com>
2022-06-27 09:18:29 -0300

    chore: carbon ads on doc page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,12 @@ module.exports = {
   base: '/v8n/',
   title: 'v8n',
   description: 'The ultimate JavaScript validation library',
+  theme: 'carbon',
   themeConfig: {
+    carbonAds: {
+      serve: 'CEAIV53L',
+      placement: 'imbrngithubio',
+    },
     nav: [
       { text: 'Guide', link: '/' },
       { text: 'API', link: '/api/' },
